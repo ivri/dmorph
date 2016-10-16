6814489fa2028154ab1cf45ecb2333f5072a76a3
ivri <evylomova@gmail.com>
2016-10-17 07:22:38 +1100

    refactored

--- deriv.cc
+++ deriv.cc
@@ -1,15 +1,270 @@
 #include "deriv.h"
+#include <tuple>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <ctime>
+
+#include "cnn/gru.h"
+
+#include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+using namespace std;
+using namespace cnn;
+using namespace boost::program_options;
+using namespace boost::filesystem;
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 
+cnn::Dict d, dc;
+int kSOS, kEOS, kSOW, kEOW;
+
+//parameters
+unsigned LAYERS = 3;
+unsigned EMBEDDING_DIM = 200;
+unsigned EMBEDDING_CHAR_DIM = 15;
+unsigned HIDDEN_DIM = 200;
+unsigned INPUT_VOCAB_SIZE = 0;
+unsigned INPUT_LEX_SIZE = 0;
+
+template <class rnn_t> int main_body(variables_map vm);
+void initialise(Model &model, const string &filename);
+void ReadFile(const char* fname, vector<VocabEntryPtr>& dataset);
 
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	cnn::initialize(argc, argv);
-	if (argc != 3 && argc != 4) {
-		cerr << "Usage: " << argv[0] << " corpus.txt dev.txt [model.params]\n";
+
+	// command line processing
+	variables_map vm;
+	options_description opts("Allowed options");
+	opts.add_options()
+		("help", "print help message")
+		("input,i", value<string>(), "file containing training sentences. Either a single file with "
+		 "each line consisting of source ||| target ||| align; or ")
+		("devel,d", value<string>(), "file containing development sentences (see --input)")
+		("test", value<string>(), "file containing test sentences (see -- input)")
+		("initialise", value<string>(), "file containing the saved model parameters")
+		("sup-train", "run training of the model")
+		("threshold-src,s", value<int>(), "keep only the <num> most frequent words (source)")
+		("treport,r", value<int>(), "report training every i iterations")
+		("dreport,R", value<int>(), "report dev every i iterations")
+		("batch_size", value<int>(), "batch size in unsupervised learning")
+		("batch_iter", value<int>(), "max batch iterations in unsup learning")
+		("epochs,e", value<int>(), "max number of epochs")
+		("layers,l", value<int>()->default_value(LAYERS), "use <num> layers for RNN components")
+		("embedding,E", value<int>()->default_value(EMBEDDING_DIM), "use <num> dimensions for word embeddings")
+		("part-embedding,P", value<int>()->default_value(EMBEDDING_CHAR_DIM), "use <num> dimensions for character embeddings")
+		("hidden,h", value<int>()->default_value(HIDDEN_DIM), "use <num> dimensions for recurrent hidden states")
+		("gru", "use Gated Recurrent Unit (GRU) for recurrent structure; default RNN")
+		("lstm", "use Long Short Term Memory (GRU) for recurrent structure; default RNN")
+		("decode", "decode sentences in the test set")
+		;
+	store(parse_command_line(argc, argv, opts), vm);
+
+	notify(vm);
+
+	bool valid_command=vm.count("sup-train") || vm.count("decode");
+
+	if (vm.count("help") || (! valid_command ) ) {
+		cout << opts << "\n";
 		return 1;
 	}
-	LoadData(argv);
+
+	if (vm.count("lstm")) {
+		cout << "%% Using LSTM recurrent units" << endl;
+		return main_body<LSTMBuilder>(vm);
+	} else if (vm.count("gru")) {
+		cout << "%% Using GRU recurrent units" << endl;
+		return main_body<GRUBuilder>(vm);
+	} else {
+		cout << "%% Using Simple RNN recurrent units" << endl;
+		return main_body<SimpleRNNBuilder>(vm);
+	}
 }
+
+template <class rnn_t>
+int main_body(variables_map vm)
+{
+	unsigned MAX_EPOCH = 100;
+	unsigned WRITE_EVERY_I = 1000;
+	unsigned report = 1000;
+
+	unsigned batch_size = 10;
+	unsigned batch_iter = 2;
+
+	kSOS = d.convert("<s>");
+	kEOS = d.convert("</s>");
+	kSOW = dc.convert("{");
+	kEOW = dc.convert("}");
+
+	//----
+	if (vm.count("batch_size")) batch_size = vm["batch_size"].as<int>();
+	if (vm.count("batch_iter")) batch_iter = vm["batch_iter"].as<int>();
+
+	if (vm.count("dreport")) WRITE_EVERY_I = vm["dreport"].as<int>();
+	if (vm.count("treport")) report = vm["treport"].as<int>();
+	if (vm.count("epochs")) MAX_EPOCH = vm["epochs"].as<int>();
+
+	if (vm.count("layers")) LAYERS = vm["layers"].as<int>();
+	if (vm.count("embedding")) EMBEDDING_DIM = vm["embedding"].as<int>();
+	if (vm.count("hidden")) HIDDEN_DIM = vm["hidden"].as<int>();
+	if (vm.count("part-embedding")) EMBEDDING_CHAR_DIM = vm["part-embedding"].as<int>();
+
+	// ---- read training sentences
+	vector<VocabEntryPtr> training;
+	Model model;
+	if (vm.count("input")) {
+		if (! exists(vm["input"].as<string>())) {
+			cout << "the input file doesnt exist" << endl;
+			return 1;
+		}
+		ReadFile(vm["input"].as<string>().c_str(), training);
+	}
+
+	d.freeze();
+	d.set_unk("UNK");
+	dc.freeze();
+
+	INPUT_VOCAB_SIZE = d.size();
+	INPUT_LEX_SIZE = dc.size();
+
+	if (vm.count("sup-train")) {
+		vector<VocabEntryPtr> devel;
+
+		if (vm.count("devel")) {
+			if (! exists(vm["devel"].as<string>())) {
+				cout << "the input file doesnt exist" << endl;
+				return 1;
+			}
+			ReadFile(vm["devel"].as<string>().c_str(), devel);
+		}
+
+		// ---- output vocab, corpus stats
+		cout << "%% Training has " << training.size() << " sentence pairs\n";
+		cout << "%% Development has " << devel.size() << " sentence pairs\n";
+		cout << "%% source vocab " << INPUT_VOCAB_SIZE << " unique words\n";
+
+		cout << "%% batch size in unsupervised learning: " << batch_size << endl;
+		cout << "%% batch max iterations in unsup learning:" << batch_iter << endl;
+
+		//---- file name for saving the parameters
+		ostringstream os;
+		os << "lm"
+			<< '_' << LAYERS
+			<< "_w" << EMBEDDING_DIM
+			<< "_h" << HIDDEN_DIM
+			<< "_c"<< EMBEDDING_CHAR_DIM
+			<< '_' << ((vm.count("lstm")) ? "lstm" : (vm.count("gru")) ? "gru" : "rnn")
+			<< "-pid" << getpid() << ".params";
+		const string fname = os.str();
+		cerr << "Parameters will be written to: " << fname << endl;
+
+		cerr << "%% layers " << LAYERS << " embedding " << EMBEDDING_DIM << " hidden " << HIDDEN_DIM << endl;
+		cerr << "%%  Character embedding dimensionality is set to " << EMBEDDING_CHAR_DIM << endl;
+
+
+		bool use_momentum = true;
+		Trainer* sgd = nullptr;
+		if (use_momentum)
+			sgd = new MomentumSGDTrainer(&model);
+		else
+			sgd = new SimpleSGDTrainer(&model);
+		EncoderDecoder<rnn_t> lm(model, LAYERS, EMBEDDING_DIM, HIDDEN_DIM, EMBEDDING_CHAR_DIM, INPUT_VOCAB_SIZE, INPUT_LEX_SIZE);
+		sup_train<rnn_t>(training, devel, &model,  &lm, report, WRITE_EVERY_I, sgd, fname);
+	}
+
+
+	if (vm.count("decode")) {
+		EncoderDecoder<rnn_t> lm(model, LAYERS, EMBEDDING_DIM, HIDDEN_DIM, EMBEDDING_CHAR_DIM, INPUT_VOCAB_SIZE, INPUT_LEX_SIZE);
+
+		if (vm.count("initialise")) {
+                	cerr << "initialising the model from: " << vm["initialise"].as<string>() << endl;
+                	initialise(model, vm["initialise"].as<string>());
+        	}
+		else
+			return 1;
+
+		vector<VocabEntryPtr> test;
+		cerr << "Reading test data from " << vm["test"].as<string>() << "...\n";
+		if (vm.count("test")) {
+			if (! exists(vm["test"].as<string>())) {
+				cout << "the test file doesnt exist" << endl;
+				return 1;
+			}
+			ReadFile(vm["test"].as<string>().c_str(), test);
+		}
+
+		run_decode<rnn_t>(test, &lm);
+	}
+
+}
+
+
+
+
+void initialise(Model &model, const string &filename)
+{
+	cerr << "Initialising model parameters from file: " << filename << endl;
+	ifstream in(filename);
+	boost::archive::text_iarchive ia(in);
+	ia >> model;
+}
+
+
+// { a l i g n } ||| { a l i g n m e n t } ||| { a l i g n m e n t s } ||| <s> VIOLENT groups use rage and weapons to their advantage and sometimes terrorism PEACEFUL ||| depict pacifist and anti-war movements </s>
+
+void ReadFile(const char* fname, vector<VocabEntryPtr>& dataset)
+{
+	std::string line, token;
+
+	ifstream in(fname);
+	if(!in.is_open()){
+		cerr << "Failed to open file " << fname << endl;
+		exit(1);
+	}
+
+	assert(in);
+
+	unsigned tlc=0;
+	while (getline(in, line)) {
+		VocabEntryPtr entry(new VocabEntry());
+		istringstream ss(line.c_str());
+		int state = 0;
+		while (ss >> token)
+		{
+			if (token == "|||"){
+				state += 1;
+				assert(state <= 4);
+				continue;
+			}
+
+
+			if (state == 0)
+				entry->Base.push_back(dc.convert(token));
+			else if (state == 1)
+				entry->Derived.push_back(dc.convert(token));
+			else if (state == 2)   // skipping the word form
+				continue;
+			else if (state == 3)
+				entry->LeftContext.push_back(d.convert(token));
+			else
+				entry->RightContext.push_back(d.convert(token));
+		}
+			if (entry->LeftContext.front() != kSOS && entry->RightContext.back() != kEOS) {
+				cerr << "The sentence in " << fname << ":" << tlc << " didn't start or end with <s>, </s>\n";
+				abort();
+			}
+		++tlc;
+		dataset.push_back(entry);
+	}
+
+	cerr << tlc << " lines, " <<  d.size() << " types, " << dc.size() << " chars\n" ;
+}
+
--- deriv.h
+++ deriv.h
@@ -40,19 +40,6 @@ struct VocabEntry
 
 typedef boost::shared_ptr<VocabEntry> VocabEntryPtr;
 
-cnn::Dict d, dc;
-int kSOS,kEOS;
-
-//parameters
-unsigned LAYERS = 3;
-unsigned INPUT_DIM = 200;
-unsigned INPUT_CHAR_DIM = 15;
-unsigned HIDDEN_DIM = 200;
-unsigned INPUT_VOCAB_SIZE = 0;
-unsigned INPUT_LEX_SIZE = 0;
-
-int kSOW;
-int kEOW;
 
 template <class Builder>
 struct EncoderDecoder {
@@ -70,8 +57,13 @@ struct EncoderDecoder {
 	Builder right_rev_enc_builder;
 	Builder right_fwd_enc_builder; 
 	Builder base_rev_enc_builder;
-	Builder base_fwd_enc_builder; 
-	explicit EncoderDecoder(Model& model) :
+	Builder base_fwd_enc_builder;
+	unsigned layers;
+        unsigned embedding_dim;
+        unsigned hidden_dim;
+        unsigned num_init_params;
+ 
+	explicit EncoderDecoder(Model& model, unsigned LAYERS, unsigned INPUT_DIM, unsigned HIDDEN_DIM, unsigned INPUT_CHAR_DIM, unsigned INPUT_VOCAB_SIZE, unsigned INPUT_LEX_SIZE) :
 		dec_builder(LAYERS, INPUT_CHAR_DIM, HIDDEN_DIM, &model),
 		left_rev_enc_builder(LAYERS, INPUT_DIM, HIDDEN_DIM, &model),
 		left_fwd_enc_builder(LAYERS, INPUT_DIM, HIDDEN_DIM, &model),
@@ -79,7 +71,7 @@ struct EncoderDecoder {
 		right_fwd_enc_builder(LAYERS, INPUT_DIM, HIDDEN_DIM, &model),
 		base_rev_enc_builder(LAYERS, INPUT_CHAR_DIM, HIDDEN_DIM, &model),
 		base_fwd_enc_builder(LAYERS, INPUT_CHAR_DIM, HIDDEN_DIM, &model){
-			p_ie2h = model.add_parameters({unsigned(HIDDEN_DIM * LAYERS * 1.5), unsigned(HIDDEN_DIM * LAYERS * 6)});
+			p_ie2h = model.add_parameters({unsigned(HIDDEN_DIM * LAYERS * 1.5), unsigned(HIDDEN_DIM * LAYERS * 6)}); //6
 			p_bie = model.add_parameters({unsigned(HIDDEN_DIM * LAYERS * 1.5)});
 			p_h2oe = model.add_parameters({unsigned(HIDDEN_DIM * LAYERS), unsigned(HIDDEN_DIM * LAYERS * 1.5)});
 			p_boe = model.add_parameters({unsigned(HIDDEN_DIM * LAYERS)});
@@ -87,13 +79,17 @@ struct EncoderDecoder {
 			p_ec = model.add_lookup_parameters(INPUT_VOCAB_SIZE, {INPUT_DIM}); 
 			p_R = model.add_parameters({INPUT_LEX_SIZE, HIDDEN_DIM});
 			p_bias = model.add_parameters({INPUT_LEX_SIZE});
+			
+			this->layers = LAYERS;
+			this->hidden_dim = HIDDEN_DIM;
 		}
 
 	// build graph and return Expression for total loss
-	Expression BuildGraph(const vector<int>& leftsent, const vector<int>& rightsent,const vector<int>& base, const vector<int>& derived, ComputationGraph& cg) {
+	vector<Expression> BuildGraph(const vector<int>& leftsent, const vector<int>& rightsent,const vector<int>& base, ComputationGraph& cg) {
 
 		// LEFT CONTEXT
 		// forward encoder for left context
+
 		left_fwd_enc_builder.new_graph(cg);
 		left_fwd_enc_builder.start_new_sequence();
 		for (unsigned t = 0; t < leftsent.size(); ++t) {
@@ -162,13 +158,19 @@ struct EncoderDecoder {
 		Expression i_nc = i_boe + i_h2oe * i_h;
 
 		vector<Expression> oein1, oein2, oein;
-		for (unsigned i = 0; i < LAYERS; ++i) {
-			oein1.push_back(pickrange(i_nc, i * HIDDEN_DIM, (i + 1) * HIDDEN_DIM));
+		for (unsigned i = 0; i < layers; ++i) {
+			oein1.push_back(pickrange(i_nc, i * hidden_dim, (i + 1) * hidden_dim));
 			oein2.push_back(tanh(oein1[i]));
 		}
-		for (unsigned i = 0; i < LAYERS; ++i) oein.push_back(oein1[i]);
-		for (unsigned i = 0; i < LAYERS; ++i) oein.push_back(oein2[i]);
+		for (unsigned i = 0; i < layers; ++i) oein.push_back(oein1[i]);
+		for (unsigned i = 0; i < layers; ++i) oein.push_back(oein2[i]);
 
+		return oein;
+	}
+
+	Expression Propagate(const vector<int>& leftsent, const vector<int>& rightsent, const vector<int>& base, const vector<int>& derived, ComputationGraph& cg) {
+
+		vector<Expression> oein = BuildGraph(leftsent, rightsent, base, cg);
 		dec_builder.new_graph(cg);
 		dec_builder.start_new_sequence(oein);
 
@@ -180,126 +182,118 @@ struct EncoderDecoder {
 		const unsigned derlen = derived.size() - 1;
 		for (unsigned t = 0; t < derlen; ++t) {
 			Expression i_x_t = lookup(cg, p_c, derived[t]);
+			//			cerr << derived[t+1] << "->";
 			Expression i_y_t = dec_builder.add_input(i_x_t);
 			Expression i_r_t = i_bias + i_R * i_y_t;
 			Expression i_ydist = log_softmax(i_r_t);
+			//			cg.incremental_forward();
+			//			auto dist = as_vector(cg.get_value(i_ydist));
+			//      for (auto item = dist.begin(); item != dist.end(); ++item)
+			//              std::cout << *item << ' ';
+			//                        next = std::max_element(dist.begin(), dist.end()) - dist.begin();
+			//                        cout << dc.convert(next)  << " ";
+
+			//errs.push_back(pickneglogsoftmax(i_r_t, derived[t+1]));
 			errs.push_back(pick(i_ydist,derived[t+1]));
+			//			cerr << " " << dc.convert(derived[t+1]) << " Error= "<<as_scalar(cg.get_value(errs[t]));
 		}
+		//		cerr << "Fin!" << endl;
 		Expression i_nerr = sum(errs);
 		return -i_nerr;
 	}
-};
 
+
+	// build graph and return Expression for total loss
+	vector<int> Decode(const vector<int>& leftsent, const vector<int>& rightsent, const vector<int>& base, ComputationGraph& cg) {
+
+		vector<Expression> oein = BuildGraph(leftsent, rightsent, base, cg);
+		dec_builder.new_graph(cg);
+		dec_builder.start_new_sequence(oein);
+		int next = 0;//kSOW;
+		// decoder
+		Expression i_R = parameter(cg,p_R);
+		Expression i_bias = parameter(cg,p_bias);
+		vector<int> result;
+		//int t = 0;
+		//		cout << "=====================" << endl;
+		do {
+			Expression i_x_t = lookup(cg, p_c, next);
+			Expression i_y_t = dec_builder.add_input(i_x_t);
+			Expression i_r_t = i_bias + i_R * i_y_t;
+			Expression i_ydist = log_softmax(i_r_t);
+			auto dist = as_vector(cg.incremental_forward());
+			next = std::max_element(dist.begin(), dist.end()) - dist.begin();
+			//			for (auto item = dist.begin(); item != dist.end(); ++item)
+			//                                std::cout << *item << ' ';
+			//			cout << "\nnext = "<< dc.convert(next) << "  i=" << i << endl;
+			result.push_back(next);
+			//	errs.push_back(pick(dist,derived[t+1]));
+			//	++i;
+		} 
+		while (next != 1);//kEOW );//&& i != 20); //EOW
+
+		return result;
+	}
+};
+/*
 // { a l i g n } ||| { a l i g n m e n t } ||| { a l i g n m e n t s } ||| <s> VIOLENT groups use rage and weapons to their advantage and sometimes terrorism PEACEFUL ||| depict pacifist and anti-war movements </s>
 
 void ReadFile(const char* fname, vector<VocabEntryPtr>& dataset)
 {
-	std::string line, token;
+std::string line, token;
 
-	ifstream in(fname);
-	if(!in.is_open()){
-		cerr << "Failed to open file " << fname << endl;
-		exit(1);
-	}
-
-	assert(in);
-
-	unsigned tlc=0;
-	while (getline(in, line)) {
-		VocabEntryPtr entry(new VocabEntry());
-		istringstream ss(line.c_str());
-		int state = 0;
-		while (ss >> token)
-		{
-			if (token == "|||"){
-				state += 1;
-				assert(state <= 4);
-				continue;
-			}
-
-
-			if (state == 0)
-				entry->Base.push_back(dc.convert(token));
-			else if (state == 1)
-				entry->Derived.push_back(dc.convert(token));
-			else if (state == 2)   // skipping the word form
-				continue;
-			else if (state == 3)
-				entry->LeftContext.push_back(d.convert(token));
-			else
-				entry->RightContext.push_back(d.convert(token));
-		}
-		if (entry->LeftContext.front() != kSOS && entry->RightContext.back() != kEOS) {
-			cerr << "The sentence in " << fname << ":" << tlc << " didn't start or end with <s>, </s>\n";
-			abort();
-		}
-		++tlc;
-		dataset.push_back(entry);
-	}
-
-	cerr << tlc << " lines, " <<  d.size() << " types, " << dc.size() << " chars\n" ;
+ifstream in(fname);
+if(!in.is_open()){
+cerr << "Failed to open file " << fname << endl;
+exit(1);
 }
-void LoadData(const char*const* argv)
+
+assert(in);
+
+unsigned tlc=0;
+while (getline(in, line)) {
+VocabEntryPtr entry(new VocabEntry());
+istringstream ss(line.c_str());
+int state = 0;
+while (ss >> token)
 {
-	kSOS = d.convert("<s>");
-	kEOS = d.convert("</s>");
-	kSOW = dc.convert("{");
-	kEOW = dc.convert("}");
-	vector<VocabEntryPtr> training, dev;
+if (token == "|||"){
+state += 1;
+assert(state <= 4);
+continue;
+}
 
-	Model model;
-	cerr << "Reading training data from " << argv[1] << "...\n";
-	ReadFile(argv[1], training);
 
-	//	if (argv[2]){
-	//	unsigned lid = 0;
-	//       auto &lparams = model.lookup_parameters_list();
-	//        for (const auto &p : model.lookup_parameters_list())  {
-	//            for (unsigned i = 0; i < p->values.size(); ++i)
-	//               memcpy(lparams[lid]->values[i].v, &p->values[i].v[0], sizeof(cnn::real) * p->values[i].d.size());
-	//            lid++;
-	//       	}
-	//	}
-	d.freeze();
-	d.set_unk("UNK");
-	dc.freeze();
+if (state == 0)
+entry->Base.push_back(dc.convert(token));
+else if (state == 1)
+entry->Derived.push_back(dc.convert(token));
+else if (state == 2)   // skipping the word form
+continue;
+else if (state == 3)
+entry->LeftContext.push_back(d.convert(token));
+else
+entry->RightContext.push_back(d.convert(token));
+}
+if (entry->LeftContext.front() != kSOS && entry->RightContext.back() != kEOS) {
+cerr << "The sentence in " << fname << ":" << tlc << " didn't start or end with <s>, </s>\n";
+abort();
+}
+++tlc;
+dataset.push_back(entry);
+}
 
-	cerr << "Reading development data from " << argv[2] << "...\n";
-	ReadFile(argv[2], dev);
+cerr << tlc << " lines, " <<  d.size() << " types, " << dc.size() << " chars\n" ;
+}
 
-	INPUT_VOCAB_SIZE = d.size();
-	INPUT_LEX_SIZE = dc.size();
-	ostringstream os;
-	os << "bilm"
-		<< '_' << LAYERS
-		<< '_' << INPUT_DIM
-		<< '_' << HIDDEN_DIM
-		<< "-pid" << getpid() << ".params";
-	const string fname = os.str();
-	cerr << "Parameters will be written to: " << fname << endl;
+ */
+template <class Builder>
+void sup_train(const vector<VocabEntryPtr> &training, const vector<VocabEntryPtr> &devel,Model *model,
+		EncoderDecoder<Builder> *lm, unsigned report_every_i, unsigned dev_every_i_reports, Trainer* sgd,
+		const string &fname)
+{
 	double best = 9e+99;
 
-	bool use_momentum = false;
-	Trainer* sgd = nullptr;
-	if (use_momentum)
-		sgd = new MomentumSGDTrainer(&model);
-	else
-		sgd = new SimpleSGDTrainer(&model);
-
-
-	//RNNBuilder rnn(LAYERS, INPUT_DIM, HIDDEN_DIM, &model);
-	//EncoderDecoder<SimpleRNNBuilder> lm(model);
-	EncoderDecoder<LSTMBuilder> lm(model);
-	//	if (argc == 4) {
-	//		string fname = argv[3];
-	//		ifstream in(fname);
-	//		boost::archive::text_iarchive ia(in);
-	//		ia >> model;
-	//	}
-
-
-	unsigned report_every_i = 50;// 50!
-	unsigned dev_every_i_reports = 10;
 	unsigned si = training.size();
 	vector<unsigned> order(training.size());
 	for (unsigned i = 0; i < order.size(); ++i) order[i] = i;
@@ -323,7 +317,7 @@ void LoadData(const char*const* argv)
 			auto entry = training[order[si]];
 			chars += entry->Derived.size() - 1;
 			++si;
-			lm.BuildGraph(entry->LeftContext, entry->RightContext, entry->Base, entry->Derived, cg);
+			lm->Propagate(entry->LeftContext, entry->RightContext, entry->Base, entry->Derived, cg);
 			loss += as_scalar(cg.forward());
 			cg.backward();
 			sgd->update();
@@ -342,20 +336,45 @@ void LoadData(const char*const* argv)
 		if (report % dev_every_i_reports == 0) {
 			double dloss = 0;
 			int dchars = 0;
-			for (auto entry : dev) {
+			for (auto entry : devel) {
 				ComputationGraph cg;
-				lm.BuildGraph(entry->LeftContext, entry->RightContext, entry->Base, entry->Derived, cg);
+				lm->Propagate(entry->LeftContext, entry->RightContext, entry->Base, entry->Derived, cg);
+				//					for (std::vector<int>::const_iterator item = entry->Base.begin(); item != entry->Base.end(); ++item)
+				//		                                std::cout << dc.convert(*item) << ',';
+
 				dloss += as_scalar(cg.forward());
 				dchars += entry->Derived.size() - 1;
 			}
 			if (dloss < best) {
 				best = dloss;
 				ofstream out(fname);
+				cerr << "Saving the model into the file...." << endl;
 				boost::archive::text_oarchive oa(out);
-				oa << model;
+				oa << *model;
 			}
-			cerr << "\n***DEV [epoch=" << (lines / (double)training.size()) << "] E = " << (dloss / dchars) << " ppl=" << exp(dloss / dchars) << ' ';
+			cerr << "\n***DEV [epoch=" << (lines / (double)training.size()) << "] Loss = " << dloss << " Chars = " << dchars  <<" E = " << (dloss / dchars) << " ppl=" << exp(dloss / dchars) << ' ';
 		}
+	}
+}
+	template <class Builder>
+void run_decode(const vector<VocabEntryPtr> &test, EncoderDecoder<Builder> *lm)
+{
+	double loss = 0;
+	double chars = 0;
+	for (auto entry : test) {
+		cout << "\n";
+		ComputationGraph cg;
+		for (std::vector<int>::const_iterator item = entry->Derived.begin(); item != entry->Derived.end(); ++item)
+			std::cout << *item << ' ';//dc.convert(*item) << ' ';
+		std::cout << "|||" ;
+		// loss += as_scalar(cg.forward());
+		// chars += entry->Derived.size() - 1;
+		std::vector<int> res = lm->Decode(entry->LeftContext, entry->RightContext, entry->Base, cg);
+		cg.forward();
+		//chars += entry->Derived.size() - 1;
+		for (std::vector<int>::const_iterator item = res.begin(); item != res.end(); ++item)
+			std::cout << *item << ' ';//dc.convert(*item) << ' '; FIXX
+		//cerr << " E = " << (loss / chars) << " ppl=" << exp(loss / chars) << ' ';
 	}
 }
 
