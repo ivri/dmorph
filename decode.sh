2683dc47d004d39213d450607112a3b841abb9fa
ivri <evylomova@gmail.com>
2016-10-17 07:20:19 +1100

    refactored

---
+++ decode.sh
@@ -0,0 +1,2 @@
+./dmorph --cnn-mem 4444 --input train --test test --initialise bilm_3_200_200-pid40838.params --layers 3 --embedding 200 --part-embedding 15 --hidden 200 --lstm --decode
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+ ./dmorph_gpu --cnn-mem 4444 --input train --devel dev --treport 100 --dreport 3 --layers 3 --embedding 200 --part-embedding 15 --hidden 200 --lstm --sup-train
